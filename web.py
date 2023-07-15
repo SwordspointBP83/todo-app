7addeede8d083207129df21106506e15750a2170
bpark <b.parker8335@gmail.com>
2023-07-14 20:56:55 -0500

    undo bold and widening change made to the web.py file

--- web.py
+++ web.py
@@ -3,7 +3,6 @@ import functions
 
 todos = functions.get_todos()
 
-st.set_page_config(layout="wide")
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
@@ -13,8 +12,7 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your <b>productivity</b>.",
-         unsafe_allow_html=True)
+st.write("This app is to increase your <b>productivity.")
 
 
 for index, todo in enumerate(todos):
